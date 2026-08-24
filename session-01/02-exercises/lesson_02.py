fa852873a4ea1862397a8c666813c8c7a46e7964
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-08-24 21:05:55 +0200

    Completed part 1

---
+++ session-01/02-exercises/lesson_02.py
@@ -0,0 +1,5 @@
+first_name = "Ronja"
+favorite_language = "Python"
+
+print("My name is", first_name)
+print("I like", favorite_language)
---
+++ session-01/02-exercises/lesson_04.py
@@ -0,0 +1,3 @@
+favorite_farge = input("Hva er din favorittfarge?")
+
+print(f"Din favorittfarge er", favorite_farge)
