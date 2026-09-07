9b22fe91f2dd42cd639deae28564ab0dcbe968b7
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-09-07 20:32:16 +0200

    Completed exercises 2.1 - 2.3

---
+++ 03-py-functions/02-exercises/2.2_create_a_dividing_line.py
@@ -0,0 +1,14 @@
+def show_seperator():
+    dividing_line = "_" * 30
+    print(dividing_line)
+
+print("Monday")
+show_seperator()
+
+print("Tuesday")
+show_seperator()
+
+print("Wednesday")
+
+
+
---
+++ 03-py-functions/02-exercises/2.3_countdown.py
@@ -0,0 +1,7 @@
+def show_countdown():
+    for countdown in range(5, 0, -1):
+        print(countdown)
+
+print("Start")
+
+show_countdown()
